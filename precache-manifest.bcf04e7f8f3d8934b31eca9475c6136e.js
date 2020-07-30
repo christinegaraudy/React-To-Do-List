b19cc356fca86d0517d6f4adb7df0fae10124fde
Christine Garaudy <christinegaraudy@gmail.com>
2020-07-30 14:43:03 -0500

    Updates

---
+++ precache-manifest.bcf04e7f8f3d8934b31eca9475c6136e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec379f7b93c621b0817df5bc7d927143",
+    "url": "/christinegaraudy/React-To-Do-List/index.html"
+  },
+  {
+    "revision": "6099c503845985818070",
+    "url": "/christinegaraudy/React-To-Do-List/static/css/main.f7186e1d.chunk.css"
+  },
+  {
+    "revision": "836a4f726c85ff01eb8f",
+    "url": "/christinegaraudy/React-To-Do-List/static/js/2.11d43a1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/christinegaraudy/React-To-Do-List/static/js/2.11d43a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6099c503845985818070",
+    "url": "/christinegaraudy/React-To-Do-List/static/js/main.c8c8be75.chunk.js"
+  },
+  {
+    "revision": "63d119f6dcec4a69ce17",
+    "url": "/christinegaraudy/React-To-Do-List/static/js/runtime-main.830dd6d3.js"
+  }
+]);
